77a2f09086e15618e85178a190e148c61f4376d4
ophir472 <ophir472@gmail.com>
2023-04-01 14:19:37 +0300

    init

---
+++ Practice/practice.c
@@ -0,0 +1,18 @@
+/* https://www.w3resource.com/c-programming-exercises/basic-2/index.a
+ 1. Write a C program that takes n number of positive integers. Find the integer that appears the least number of times among the said integers. If there are multiple such integers, select the smallest one. Go to the editor
+    Sample Date:
+        (1,2,3) -> 1
+        (10, 20, 4, 5, 11) -> 4 */
+
+int main(int argc, char const *argv[])
+{
+    int i, n;
+    printf("Please insert how many integers");
+    scanf("%d", &n);
+    for ( i = 0 ; i < n; i++){
+        
+    }
+    return 0;
+
+    int myNum;
+}
